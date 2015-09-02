02de23de55bbe05ddc8d88e201a64aab4fbbc037
Natapone C <natapone@gmail.com>
2015-09-02 11:13:03 +0700

    vagrant create DB script

--- vagrant/script/install_lamp.sh
+++ vagrant/script/install_lamp.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
 PROJECTFOLDER='project'
+DB_ROOT_PASS='devel'
+DB_USER='devel'
+DB_NAME='drupal'
 
 # Updating repository
 
 sudo apt-get -y update
 
 # Installing Apache
-
 sudo apt-get -y install apache2
-
-# Installing MySQL and it's dependencies, Also, setting up root password for MySQL as it will prompt to enter the password during installation
-
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password devel'
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password devel'
-sudo apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
 
 # Installing PHP and it's dependencies
 sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
+
+# Installing MySQL and it's dependencies, Also, setting up root password for MySQL as it will prompt to enter the password during installation
+
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password ${DB_ROOT_PASS}'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password ${DB_ROOT_PASS}'
+sudo apt-get -y install mysql-server-5.5 libapache2-mod-auth-mysql php5-mysql
+
+# Creat database
+if [ ! -f /var/log/databasesetup ];
+then
+    echo "CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}'" | mysql -uroot -p${DB_ROOT_PASS}
+    echo "CREATE DATABASE ${DB_NAME}" | mysql -uroot -p${DB_ROOT_PASS}
+    echo "GRANT ALL ON ${DB_NAME}.* TO '${DB_USER}'@'localhost'" | mysql -uroot -p${DB_ROOT_PASS}
+    echo "flush privileges" | mysql -uroot -p${DB_ROOT_PASS}
+
+    touch /var/log/databasesetup
+fi
 
 # setup hosts file
 VHOST=$(cat <<EOF
