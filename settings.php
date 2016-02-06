7374570c638a9ebfe876f39bf41b43ffcea2e60d
Natapone C <natapone@gmail.com>
2016-02-06 15:58:49 +0700

    add setting file

---
+++ settings.php
@@ -0,0 +1,20 @@
+<?php
+
+$databases['default']['default'] = array(
+    'driver' => 'mysql',
+    'database' => 'drupal',
+    'username' => 'devel',
+    'password' => 'devel',
+    'host' => 'localhost',
+    'prefix' => '',
+);
+
+$config_directories = array();
+
+$settings['hash_salt'] = '';
+
+$settings['update_free_access'] = TRUE;
+
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
+
+
