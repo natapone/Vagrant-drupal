5c1debb70383b1e5494fd668a6687cb52ad78e8b
Natapone Charsombut <natapone@gmail.com>
2019-06-08 14:57:37 +0630

    rename installation script by order

--- vagrant/script/3_install_drupal.sh
+++ vagrant/script/3_install_drupal.sh
@@ -3,8 +3,8 @@ PROJECT_PATH='/var/www/html/project'
 
 mkdir -p ${PROJECT_PATH}/sites/default/files
 # only for fresh install
-sudo chmod 777 -R ${PROJECT_PATH}/sites/default/files
-sudo chmod 777 ${PROJECT_PATH}/sites/default/settings.php
+# sudo chmod 777 -R ${PROJECT_PATH}/sites/default/files
+# sudo chmod 777 ${PROJECT_PATH}/sites/default/settings.php
 
 # For Drupal 8
 # sudo apt-get -y install nginx
